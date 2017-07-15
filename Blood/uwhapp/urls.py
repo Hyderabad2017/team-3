d1c135e760fc3c4126f7ee7c64fe6de99fb96bb3
sekharkaredla <sekhar.karedla@gmail.com>
2017-07-15 17:05:03 +0530

    registrations

--- Blood/uwhapp/urls.py
+++ Blood/uwhapp/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-    url(r'^donor_logincheck/',views.DonorLogincheck.as_view()),
+    url(r'^donor_register_detials',views.RegisterDetailsOfDonor.as_view()),
+    url(r'^donor_register',views.DonorRegister.as_view()),
+    url(r'^donor_logincheck',views.DonorLogincheck.as_view()),
     url(r'^donor',views.DonorPage.as_view()),
-    url(r'^bloodbank_logincheck/', views.BloodbankLogincheck.as_view()),
-    url(r'^bloodbank/', views.Bloodbank.as_view()),
+    url(r'^bloodbank_logincheck', views.BloodbankLogincheck.as_view()),
+    url(r'^bloodbank', views.Bloodbank.as_view()),
     url(r'', views.Index.as_view()),
 ]
--- Blood/uwhapp/views.py
+++ Blood/uwhapp/views.py
@@ -17,12 +17,12 @@ class Index(View):
 
 
 class Bloodbank(View):
-    def get(self, request):
+    def post(self, request):
         return render(request, 'uwhapp/bloodbank_login.html')
 
 
 class DonorPage(View):
-    def get(self, request):
+    def post(self, request):
         return render(request, 'uwhapp/donor_login.html')
 
 
@@ -41,6 +41,8 @@ class BloodbankLogincheck(View):
 
 
 class DonorLogincheck(View):
+    # def get_context_dict(self):
+
     def post(self, request):
         username = request.POST.get('user')
         password = request.POST.get('pass')
@@ -49,6 +51,27 @@ class DonorLogincheck(View):
             return redirect('/uwhapp/donor')
         blood_bank_obj = blood_bank_obj[0]
         if blood_bank_obj.password == password:
+            # context=self.get_context_dict()
             return render(request, 'uwhapp/donor.html')
         else:
             return redirect('/uwhapp/donor')
+
+
+class DonorRegister(View):
+    def post(self, request):
+        return render(request, 'uwhapp/donor_register.html')
+
+
+class RegisterDetailsOfDonor(View):
+    def post(self, request):
+        Donor.objects.create(
+            name=request.POST['name'],
+            userid=request.POST['userid'],
+            password=request.POST['pass'],
+            blood_type=request.POST['blood_type'],
+            age=request.POST['age'],
+            weight=request.POST['weight'],
+            haemo=request.POST['haemo'],
+            last_donation=request.POST['last_donation']
+        )
+
