f14c762cfedc1d3d189812e351d80adb3822644b
Force <force@ip-172-31-26-13.ap-southeast-1.compute.internal>
2017-07-16 03:35:45 +0000

    pushesh

--- Blood/Blood/settings.py
+++ Blood/Blood/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k-4ug8r#bs_(2u%)8m@tft%)%-u)1+vcsmbwh1%8=tlkfq)=+n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.255.241.239']
 
 
 # Application definition
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'djangoforandroid.builder',
-    'djangoforandroid.mdl',
+#    'djangoforandroid.builder',
+ #   'djangoforandroid.mdl',
     #'djangoforandroid.d4a'
 ]
 
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'uwhdb',
         'USER':'root',
-        'PASSWORD':'pandu123',
+        'PASSWORD':'team3',
         'PORT':''
     }
 }
@@ -130,5 +130,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'apk')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'apk')
 
